4fc4937b1c0a583c0e127afb0b920a767da94c50
Shouichi Nakauchi <nakauchi@mtc.biglobe.ne.jp>
2010-02-20 02:04:18 +0900

    html output

--- recorddiet.rb
+++ recorddiet.rb
@@ -1,13 +1,31 @@
+#!/usr/bin/ruby
+
 require 'pstore'
 require 'date'
+require 'cgi'
 
 def showdb(db)
-  db['root'].sort.each { |date,body|
-    print  "#{date.month}/#{date.day} :  #{body}kg\n"
-  }
+  if ARGV[2] == "-p"
+    db['root'].sort.each { |date,body|
+      print  "#{date.month}/#{date.day} :  #{body}kg\n"
+    }
+  else
+    s = ""
+    db['root'].sort.each { |date,body|
+      s += "<tr><td>#{date.month}/#{date.day}</td><td>#{body}kg</td></tr>\n"
+    }
+    cgi = CGI.new("html4")
+    cgi.out() {
+      cgi.head {cgi.title{"Recording diet"}} +
+      cgi.body() {
+        "\n<table>\n" + s + "</table>\n"
+      }
+    }
+  end
 end
 
-db = PStore.new("/Users/nakauchiaya/Documents/ruby/src/recorddiet/recorddiet.db")
+db = PStore.new("/Users/nakauchiaya/Data/recorddiet.db")
+#db = PStore.new("/Users/nakauchiaya/Documents/ruby/src/recorddiet/recorddiet.db")
 #db = PStore.new("./recorddiet.db")
 
 if !ARGV[0]
@@ -21,9 +39,6 @@ offset = ARGV[1] ? ARGV[1].to_i : 0
 date = Date.today + offset
 
 db.transaction {
-#  p date
-#  p weight
-#  p db
   db["root"] = {} unless db["root"]
   db["root"][date] = weight
 
