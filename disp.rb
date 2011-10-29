6a3ab0adc2e45a997675dc6a84dd67b2bfe5cdef
Shouichi Nakauchi <nakauchi@mtc.biglobe.ne.jp>
2011-10-30 01:24:32 +0900

    refactaring

--- disp.rb
+++ disp.rb
@@ -10,6 +10,8 @@ require "pp"
 class DispRdiet
   def initialize
     @dbn = "Data/recorddiet.db"
+
+    @graph_kind = [24, 12, 6, 3]  # 2years, 1year, 6 month and 3 month
     @graph_weight_upper = 68.0
     @graph_weight_lower = 63.0
     @graph_fat_upper = 26.0
@@ -21,7 +23,7 @@ class DispRdiet
 
   def main
     @erb = ERB.new(input_script)
-    @erb_script = ERB.new(java_script)
+    @erb_java = ERB.new(java_script)
 
     @cgi.out {
       @cgi.html() {
@@ -33,6 +35,14 @@ class DispRdiet
         } + @cgi.body { build_page }
       }
     }
+  end
+
+  def build_java_script
+    begin
+      return @erb_java.result(binding)
+    rescue
+      return faild_script
+    end
   end
 
   def java_script
@@ -48,46 +58,65 @@ class DispRdiet
 EOS
   end
 
-  def build_java_script
-    begin
-      return @erb_script.result(binding)
-    rescue
-      return faild_script
+  def mk_uri
+    uris = []
+    @graph_kind.each do |month|
+      range = month / 3
+      end_d = Date.today
+      division = Array.new(4) { |i| (end_d << (month - i * range)).to_s}
+      uris << gchart_uri(division)+mk_charts(end_d, month)
     end
+    uris
   end
 
-  def mk_charts
+  def gchart_uri(division)
+    "http://chart.apis.google.com/chart?"+
+      "chs=800x300&cht=lc&chxr=1,&chxt=x,y,r"+
+      "&chxl=0:%7C#{division[0]}%7C#{division[1]}%7C#{division[2]}%7C#{division[3]}"+
+      @graph_weight_lower.to_s+","+
+      @graph_weight_upper.to_s+","+
+      @graph_fat_lower.to_s+","+
+      @graph_fat_upper.to_s+
+      "&chdl=体重%7C脂肪率&chco=ff0000,00ff00"
+  end
+
+  def mk_charts(end_d, month)
     chart_weight = ['_']
     chart_fat = ['_']
 
     db = @db
     db.transaction do
-      @start_d.upto(@end_d) do |d|
+      (end_d << month).upto(end_d) do |d|
+
         if db["root"][d] == nil
           chart_weight << chart_weight.last
           chart_fat << chart_fat.last
+
         else
           if db["root"][d].class == Float
             # Old db format. Only weight was written in db.
             weight = db["root"][d]
-            chart_weight << 
-              (weight == 0 ? 
-               chart_weight.last : 
-               conv_gchart(weight,@graph_weight_upper, @graph_weight_lower))
-            chart_fat << "_"
+            fat = "_"
           else
             # New db foramt. [d][:weigh, :fat]
             weight = db["root"][d][:weight]
-            chart_weight << 
-              (weight == 0 ? 
-               chart_weight.last : 
-               conv_gchart(weight,@graph_weight_upper, @graph_weight_lower))
             fat = db["root"][d][:fat].to_i
-            chart_fat << 
-              (fat == 0 ? 
-               chart_fat.last :
-               conv_gchart(fat,@graph_fat_upper, @graph_fat_lower))
           end
+
+          if weight == 0
+            chart_weight << chart_weight.last
+          else
+            chart_weight << conv_gchart(weight, @graph_weight_upper, @graph_weight_lower)
+          end
+
+          if fat == 0
+            chart_fat << chart_fat.last
+          elsif fat == "_"
+            chart_fat << "_"
+          else
+            chart_fat << conv_gchart(fat, @graph_fat_upper, @graph_fat_lower)
+          end
+
         end
       end
     end
@@ -97,95 +126,8 @@ EOS
   def conv_gchart(df, upper, lower)
     range = (upper - lower).to_f
     d = ((df - lower) / range * 61).to_i
-    d = d < 0 ? 0 :
-      d > 61 ? 61 : d
-    (('A'..'Z').map + ('a'..'z').map + ('0'..'9').map)[d]
-  end
-
-  def mk_uri
-    tt = []
-
-    range = 24
-    width = range / 3
-    @start_d = Date.today << range
-    @end_d = Date.today
-    division = Array.new(4) { |i| (@end_d << (range - i * width)).to_s}
-    tt[0] = 
-      "http://chart.apis.google.com/chart?"+
-      "chs=800x300"+
-      "&cht=lc"+
-      "&chxt=x,y,r"+
-      "&chxl=0:%7C#{division[0]}%7C#{division[1]}%7C#{division[2]}%7C#{division[3]}"+
-      "&chxr=1,"+
-      @graph_weight_lower.to_s+","+
-      @graph_weight_upper.to_s+","+
-      @graph_fat_lower.to_s+","+
-      @graph_fat_upper.to_s+
-      "&chdl=体重%7C脂肪率"+
-      "&chco=ff0000,00ff00"+
-      mk_charts
-
-    range = 12
-    width = range / 3
-    @start_d = Date.today << range
-    @end_d = Date.today
-    division = Array.new(4) { |i| (@end_d << (range - i * width)).to_s}
-    tt[1] = 
-      "http://chart.apis.google.com/chart?"+
-      "chs=800x300"+
-      "&cht=lc"+
-      "&chxt=x,y,r"+
-      "&chxl=0:%7C#{division[0]}%7C#{division[1]}%7C#{division[2]}%7C#{division[3]}"+
-      "&chxr=1,"+
-      @graph_weight_lower.to_s+","+
-      @graph_weight_upper.to_s+","+
-      @graph_fat_lower.to_s+","+
-      @graph_fat_upper.to_s+
-      "&chdl=体重%7C脂肪率"+
-      "&chco=ff0000,00ff00"+
-      mk_charts
-
-    range = 6
-    width = range / 3
-    @start_d = Date.today << range
-    @end_d = Date.today
-    division = Array.new(4) { |i| (@end_d << (range - i * width)).to_s}
-    tt[2] = 
-      "http://chart.apis.google.com/chart?"+
-      "chs=800x300"+
-      "&cht=lc"+
-      "&chxt=x,y,r"+
-      "&chxl=0:%7C#{division[0]}%7C#{division[1]}%7C#{division[2]}%7C#{division[3]}"+
-      "&chxr=1,"+
-      @graph_weight_lower.to_s+","+
-      @graph_weight_upper.to_s+","+
-      @graph_fat_lower.to_s+","+
-      @graph_fat_upper.to_s+
-      "&chdl=体重%7C脂肪率"+
-      "&chco=ff0000,00ff00"+
-      mk_charts
-
-    range = 3
-    width = range / 3
-    @start_d = Date.today << range
-    @end_d = Date.today
-    division = Array.new(4) { |i| (@end_d << (range - i * width)).to_s}
-    tt[3] = 
-      "http://chart.apis.google.com/chart?"+
-      "chs=800x300"+
-      "&cht=lc"+
-      "&chxt=x,y,r"+
-      "&chxl=0:%7C#{division[0]}%7C#{division[1]}%7C#{division[2]}%7C#{division[3]}"+
-      "&chxr=1,"+
-      @graph_weight_lower.to_s+","+
-      @graph_weight_upper.to_s+","+
-      @graph_fat_lower.to_s+","+
-      @graph_fat_upper.to_s+
-      "&chdl=体重%7C脂肪率"+
-      "&chco=ff0000,00ff00"+
-      mk_charts
-
-    tt
+    _d = d < 0 ? 0 : (d > 61 ? 61 : d)
+    (('A'..'Z').map + ('a'..'z').map + ('0'..'9').map)[_d]
   end
 
   def build_page
@@ -208,15 +150,12 @@ EOS
 EOS
   end
 
-  def input_script_bak
-    <<EOS
-<h1>レコーディングダイエット</h1>
-<img src="http://chart.apis.google.com/chart?chs=800x300&cht=lc&chxt=x,y,r&chxl=0:%7C2009/10/13%7C2010/4/13%7C2010/10/13%7C2011/4/13%7C2011/10/13&chxr=1,<%= @graph_weight_lower%>,<%= @graph_weight_upper%>%7C2,<%= @graph_fat_lower%>,<%= @graph_fat_upper %>&chdl=体重%7C脂肪率<%= mk_charts %>&chco=ff0000,00ff00">
-EOS
-  end
-
   def dbg
     return eval(@erb.src, binding, __FILE__,__LINE__+4)
+  end
+
+  def dbg_java
+    return eval(@erb_java.src, binding, __FILE__,__LINE__+4)
   end
 
   def faild_script
@@ -226,11 +165,16 @@ EOS
           @cgi.title {'Recording diet'} +
           '<META HTTP-EQUIV="content-type" CONTENT="text/html;charset=utf-8">' +
           '<link rev="MADE" href="mailto:nakauchi@mtc.biglobe.ne.jp">'
-        } + @cgi.body { "Error" }
+        } + 
+        @cgi.body { 
+          "Error<p>" +
+          " " + dbg_java +
+          "<hr />" +
+          " " + dbg
+        }
       }
     }
   end
-
 end
 
 DispRdiet.new.main if __FILE__ == $0
